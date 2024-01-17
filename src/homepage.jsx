1c053da68f73f175b699ed7fb8e61cf01866798b
Deanjer <220509@student.glu.nl>
2024-01-17 11:53:47 +0100

    search-logo

--- src/homepage.jsx
+++ src/homepage.jsx
@@ -1,27 +1,30 @@
-import './homepage.css'
+import "./homepage.css";
 
-export default function HomePage(){
-    return(
-        <div className="main-container">
-            <div className="left-container">
-                
-            </div>
-            <div className="center-container">
-                <div className="center-top">
-                    <div className="center-top-left">
-                        <input className='search' type="text"  placeholder='Search'/>
-                    </div>
-                    <div className="center-top-right">
-                        <ul>
-                            <li>Acount</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div className="right-container">
-
-            </div>
-
+export default function HomePage() {
+  return (
+    <div className="main-container">
+      <div className="left-container">
+        <div className="logo">
+          <h1>
+            <span className="logo-1">React</span>
+            <span className="logo-2">Play</span>
+          </h1>
         </div>
-    );
+      </div>
+      <div className="center-container">
+        <div className="center-top">
+          <div className="center-top-left">
+            <input className="search" type="text" placeholder="Search" />
+          </div>
+          <div className="center-top-right">
+            <ul>
+              <li>Acount</li>
+            </ul>
+          </div>
+        </div>
+        {/* <hr className='center-top-underline'/> */}
+      </div>
+      <div className="right-container"></div>
+    </div>
+  );
 }
