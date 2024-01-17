171e785500bdbdc736bba172abf7e7169919f2ed
Neochees <113420296+Neochees@users.noreply.github.com>
2024-01-17 10:50:08 +0100

    Button to login done

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import login from './login'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Homepage from './Homepage';
+import Login from './Login';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <a href='./login.jsx'><button>login</button></a>
-      {/* did its een placeholder niet permanent */}
-    </>
-  )
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </Router>
+  );
 }
 
-export default App
+export default App;
