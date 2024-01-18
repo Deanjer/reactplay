6f9a1a4834a5d181301a80a4c70b00f775bcd97b
Neochees <113420296+Neochees@users.noreply.github.com>
2024-01-18 11:24:20 +0100

    changes

--- src/homepage.jsx
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom";
-
-export default function Homepage(){
-    return (
-        <div className="homrpage">
-           <Link to="login"><button>Login</button></Link>
-        </div>
-    );
-}
--- src/login.jsx
+++ src/login.jsx
@@ -1,5 +1,15 @@
 import './login.css';
 
+import { Link } from "react-router-dom";
+
+// export default function Homepage(){
+//     return (
+//         <div className="homrpage">
+//            <Link to="login"><button>Login</button></Link>
+//         </div>
+//     );
+// }
+
 export default function Login() {
     return (
         <>
