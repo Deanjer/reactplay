9d8edfa57dcf1d09cb5ba0dd4d27c3b107b74b8c
Neochees <113420296+Neochees@users.noreply.github.com>
2024-01-24 14:00:56 +0100

    progress I guess for player selector

---
+++ src/text.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PlayerSelector from './PlayerSelector';
+
+const Test = () => {
+  return (
+    <div>
+      <h1>My Spotify App</h1>
+      <PlayerSelector />
+    </div>
+  );
+};
+
+export default Test;
